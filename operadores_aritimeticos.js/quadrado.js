cc10aa7a5929fbe23f92611b1ab885c974e71c98
Emilio <favorettoemilio@gmail.com>
2025-03-28 11:10:42 -0300

    feat(operadores-aritmeticos): adiciona cálculo de quadrado e cubo

---
+++ operadores_aritimeticos.js/quadrado.js
@@ -0,0 +1,5 @@
+let conta = 3
+let quadrado = conta * 3
+let cubo = conta ** 3
+
+console.log(quadrado, cubo)
---
+++ quadrado-e-cubo.js
@@ -0,0 +1,4 @@
+let conta = 3
+let quadrado = conta * 3
+let cubo = conta ** 3
+console.log( quadrado,  cubo )
