0938766454159697d7d1ebb5704874d01a370064
emilio <favoretto@gmail.com>
2025-03-31 11:38:39 -0300

    feat(operadores-logicos): adiciona operação AND com booleanos

--- area do retagulo.js
+++
@@ -1,5 +0,0 @@
-let base = 15
-let altura = 20
-let area = base * altura
-
-console.log(" a area do terreno é " + area + " metros ")
---
+++ operadoreslogicos.js/AND_em_ação.js
@@ -0,0 +1,8 @@
+let tenhoBatata = true;
+let queroBatata = false;
+
+if (tenhoBatata == true && queroBatata == true) {
+    console.log("vamos fazer batata");
+}else {
+    console.log("não vamos fazer batata")
+}
