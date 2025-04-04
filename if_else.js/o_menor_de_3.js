d1d767dd5f557385d46d5789189b25c4a48724e7
emilio <favorettoemilio@gmail.com>
2025-04-04 11:10:17 -0300

    feat(if-else): adiciona identificação do menor entre três números

---
+++ if_else.js/o_menor_de_3.js
@@ -0,0 +1,13 @@
+let n1 = 10
+let n2 = 20
+let n3 = 25
+
+if (n1 < n2 && n3){
+    console.log("n1 é o menor número")
+}
+else if (n2 < n1 && n3){
+    console.log("n2 é o menor número")
+}
+else if (n3 < n1 && n2){
+    console.log("n3 é o menor número")
+}
