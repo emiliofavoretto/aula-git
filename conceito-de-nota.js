9917356594a8aede62ab80ddc6a30fae69afcf4b
emilio <favorettoemilio@gmail.com>
2025-04-04 11:46:14 -0300

    feat(switch-case): adiciona atribuição de conceito por nota

---
+++ conceito-de-nota.js
@@ -0,0 +1,19 @@
+let nota = 10
+
+switch(true){
+    case nota <= 10:
+        console.log("otimo")
+        break
+    case nota <= 8:
+        console.log("bom")
+        break
+    case nota <= 6:
+        console.log("ok")
+        break
+    case nota <= 4:
+        console.log("ruim")
+        break
+    case nota <= 2:
+        console.log("pessimo")
+        break
+}
