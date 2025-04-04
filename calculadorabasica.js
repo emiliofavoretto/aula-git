77e59cd9e52e5edeb6302c03a33a7bad65b6b9ee
emilio <favorettoemilio@gmail.com>
2025-04-04 11:36:46 -0300

    feat(switch-case): adiciona calculadora básica com operações

---
+++ calculadorabasica.js
@@ -0,0 +1,18 @@
+let n1 = 10
+let n2 = 2
+let conta = 'multiplicação'
+
+switch(conta){
+    case 'adição':
+        console.log(n1 + n2)
+        break
+    case 'subtração':
+        console.log(n1 - n2)
+        break
+    case 'multiplicação':
+        console.log(n1 * n2)
+        break
+    case 'divisão':
+        console.log(n1 / n2)
+        break
+}
