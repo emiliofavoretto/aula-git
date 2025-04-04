fa03c486aaea0895acc4462603e5d017a3bf7f03
emilio <favorettoemilio@gmail.com>
2025-04-04 10:26:40 -0300

    feat(if-else): adiciona classificação etária por idade

---
+++ if_else.js/classificacaoetaria.js
@@ -0,0 +1,14 @@
+let idade = 25
+
+if (idade <= 12){
+    console.log("é criança")
+}
+else if (idade <= 17){
+    console.log("é adolecente")
+}
+else if (idade => 18){
+    console.log("é adulto")
+}
+else if (idade => 60){
+    console.log("é idoso")
+}
