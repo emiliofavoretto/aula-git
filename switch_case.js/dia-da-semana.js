3d937ef04d64a5766eb3a9679ceb1a92fd3aa739
emilio <favorettoemilio@gmail.com>
2025-04-04 11:24:09 -0300

    feat(switch-case): adiciona exibição do dia da semana

---
+++ switch_case.js/dia-da-semana.js
@@ -0,0 +1,24 @@
+let dia = 3
+switch(dia){
+    case 1:
+        console.log("domingo")
+        break
+    case 2:
+        console.log("segunda")
+        break
+    case 3:
+        console.log("terça")
+        break
+    case 4:
+        console.log("quarta")
+        break
+    case 5:
+        console.log("quinta")
+        break
+    case 6:
+        console.log("sexta")
+        break
+        default:
+            console.log("sabado")
+    
+}
