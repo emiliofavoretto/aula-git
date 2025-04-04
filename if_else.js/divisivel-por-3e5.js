490bf1bf4b04e734eaca7985491585101c1e1d5c
emilio <favorettoemilio@gmail.com>
2025-04-04 10:39:38 -0300

    feat(if-else): adiciona verificação de divisibilidade por 3 e 5

---
+++ if_else.js/divisivel-por-3e5.js
@@ -0,0 +1,9 @@
+let n1 = 20
+
+if ( n1 % 3 == 0){
+    console.log("é divisivel por 3")
+}
+else if (n1 % 5 == 0){
+    console.log("é divisivel por 5")
+}
+else (console.log ("não é divisivel por nem um"))
