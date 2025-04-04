027d4119b3c7993daf11ddca76a9931c60340c61
emilio <favorettoemilio@gmail.com>
2025-04-04 11:52:57 -0300

    feat(switch-case): adiciona exibição da estação do ano

---
+++ estação-do-ano.js
@@ -0,0 +1,17 @@
+let estação = 2
+
+switch(estação){
+    case estação = 1: 
+    console.log("verão")
+    break
+    case estação = 2: 
+    console.log("outono")
+    break
+    case estação = 3: 
+    console.log("inverno")
+    break
+    case estação = 4:
+        break
+        default: 
+    console.log("primavera")
+}
